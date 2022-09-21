ccd700be405723d1c27e3578efbcb5d4f3ffeee4
ozenilsoncruz <ozenilsonalisson@gmail.com>
2022-09-21 10:13:21 -0300

    atualizacao no display

--- in_out.s
+++ in_out.s
@@ -1,24 +1,27 @@
 /* Implementacao dos metodos de 
    Leitura (input) e Escrita (print) na tela */
 
-.global _start
 
-_start:
+.macro input
     mov r0, #0            @ atribui 0 a r0 para receber dados do teclado
     mov r7, #3            @ chamada de sistema para leitura
     ldr r1, =texto        @ dado recebido
     ldr r2, =len_texto    @ tamanho da palavra lida
+    
+    mov r7, #1          @ termina os processos
     swi 0
+.endm
 
-print:
+.macro print
     mov r0, #1          @ atribui 1 ao r0 para escrever na tela
     mov r7, #4          @ chamada de sistema para escrita
     ldr r1, =texto      @ dado para impressao
     ldr r2, =len_texto  @ tamanho da palavra a ser exidida
 
-end:
     mov r7, #1          @ termina os processos
     swi 0
+.endm
+    
 
 .data
     texto: .asciiz "%d\n"

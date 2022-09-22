ea38b199e4967fe48c1c973dec462dbb80a2f80a
ozenilsoncruz <ozenilsonalisson@gmail.com>
2022-09-22 10:57:49 -0300

    atualizações no modulo do display

--- display_lcd.s
+++ display_lcd.s
@@ -1,3 +1,5 @@
+/* display_lcd.s */
+
 @ Constantes importantes para o programa
 .equ pagelen, 4096
 .equ setregoffset, 28
@@ -12,80 +14,80 @@
  
 
 .macro nanoSleep time
-        ldr R0,=\time
-        ldr R1,=\time
-        mov R7, #nano_sleep
+        ldr r0,=\time
+        ldr r1,=\time
+        mov r6, #nano_sleep
         svc 0
 .endm
 
 
 .macro GPIOReadRegister pin
-        mov R2, R8      @Endereço dos registradores da GPIO
-        add R2, #level  @offset para acessar o registrador do pin level 0x34 
-        ldr R2, [R2]    @ pino5, 19 e 26 ativos respectivamentes
-        ldr R3, =\pin   @ base dos dados do pino
-        add R3, #8      @ offset para acessar a terceira word
-        ldr R3, [R3]    /* carrega a posiçao do pino -> 
+        mov r2, r8      @Endereço dos registradores da GPIO
+        add r2, #level  @offset para acessar o registrador do pin level 0x34 
+        ldr r2, [r2]    @ pino5, 19 e 26 ativos respectivamentes
+        ldr r3, =\pin   @ base dos dados do pino
+        add r3, #8      @ offset para acessar a terceira word
+        ldr r3, [r3]    /* carrega a posiçao do pino -> 
                           ex queremos saber o valor do pino5 =2^5= 32 => 00 000 000 000 000 000 000 000 000 100 000*/
-        and R0, R2, R3  @ Filtrando os outros bits => 00 000 000 000 000 000 000 000 000 100 000
+        and r0, r2, r3  @ Filtrando os outros bits => 00 000 000 000 000 000 000 000 000 100 000
 .endm
 
 
 .macro GPIODirectionOut pin
-        ldr R2, =\pin
-        ldr R2, [R2]
-        ldr R1, [R8, R2]
-        ldr R3, =\pin @ address of pin table
-        add R3, #4 @ load amount to shift from table
-        ldr R3, [R3] @ load value of shift amt
-        mov R0, #0b111 @ mask to clear 3 bits
-        lsl R0, R3 @ shift into position
-        bic R1, R0 @ clear the three bits
-        mov R0, #1 @ 1 bit to shift into pos
-        lsl R0, R3 @ shift by amount from table
-        orr R1, R0 @ set the bit
-        str R1, [R8, R2] @ save it to reg to do work
+        ldr r2, =\pin
+        ldr r2, [r2]
+        ldr r1, [r8, r2]
+        ldr r3, =\pin @ address of pin table
+        add r3, #4 @ load amount to shift from table
+        ldr r3, [r3] @ load value of shift amt
+        mov r0, #0b111 @ mask to clear 3 bits
+        lsl r0, r3 @ shift into position
+        bic r1, r0 @ clear the three bits
+        mov r0, #1 @ 1 bit to shift into pos
+        lsl r0, r3 @ shift by amount from table
+        orr r1, r0 @ set the bit
+        str r1, [r8, r2] @ save it to reg to do work
 .endm
 
 
 .macro GPIOTurnOn pin
-        mov R2, R8 @ address of gpio regs
-        add R2, #setregoffset @ off to set reg
-        mov R0, #1 @ 1 bit to shift into pos
-        ldr R3, =\pin @ base of pin info table
-        add R3, #8 @ add offset for shift amt
-        ldr R3, [R3] @ load shift from table
-        lsl R0, R3 @ do the shift
-        str R0, [R2] @ write to the register
+        mov r2, r8 @ address of gpio regs
+        add r2, #setregoffset @ off to set reg
+        mov r0, #1 @ 1 bit to shift into pos
+        ldr r3, =\pin @ base of pin info table
+        add r3, #8 @ add offset for shift amt
+        ldr r3, [r3] @ load shift from table
+        lsl r0, r3 @ do the shift
+        str r0, [r2] @ write to the register
 .endm
 
 
 .macro GPIOTurnOff pin
-        mov R2, R8 @ address of gpio regs
-        add R2, #clrregoffset @ off set of clr reg
-        mov R0, #1 @ 1 bit to shift into pos
-        ldr R3, =\pin @ base of pin info table
-        add R3, #8
-        ldr R3, [R3]
-        lsl R0, R3
-        str R0, [R2]
+        mov r2, r8 @ address of gpio regs
+        add r2, #clrregoffset @ off set of clr reg
+        mov r0, #1 @ 1 bit to shift into pos
+        ldr r3, =\pin @ base of pin info table
+        add r3, #8
+        ldr r3, [r3]
+        lsl r0, r3
+        str r0, [r2]
 .endm
 
 
 .macro GPIOValue pin, value
-        mov R0, #40     @valor do clear off set
-        mov R2, #12     @valor que ao subtrair o clear off set resulta 28 o set
-        mov R1, \value  @registra o valor 0 ou 1 no registrador
-        mul R5, R1, R2  @Ex r1 recebe o valor 1, ou seja multiplica o 12 do r2 por 1 resultando 12 no r5
-        sub R0, R0, R5  @valor do r5 que é 12 subtraido por 40 do r0 resultando 28 para o r0 ou seja o set do offset
-        mov R2, R8      @Endereço dos registradores da GPIO
-        add R2, R2, R0  @adiciona no r2 o valor do set com o endereço dos regs
-        mov R0, #1      @ 1 bit para o shift
-        ldr R3, =\pin   @valor dos endereços dos pinos
-        add R3, #8      @ Adiciona offset para shift 
-        ldr R3, [R3]    @carrega o shift da tabela
-        lsl R0, R3      @realiza a mudança
-        str R0, [R2]    @Escreve no registro
+        mov r0, #40     @valor do clear off set
+        mov r2, #12     @valor que ao subtrair o clear off set resulta 28 o set
+        mov r1, \value  @registra o valor 0 ou 1 no registrador
+        mul r5, r1, r2  @Ex r1 recebe o valor 1, ou seja multiplica o 12 do r2 por 1 resultando 12 no r5
+        sub r0, r0, r5  @valor do r5 que é 12 subtraido por 40 do r0 resultando 28 para o r0 ou seja o set do offset
+        mov r2, r8      @Endereço dos registradores da GPIO
+        add r2, r2, r0  @adiciona no r2 o valor do set com o endereço dos regs
+        mov r0, #1      @ 1 bit para o shift
+        ldr r3, =\pin   @valor dos endereços dos pinos
+        add r3, #8      @ Adiciona offset para shift 
+        ldr r3, [r3]    @carrega o shift da tabela
+        lsl r0, r3      @realiza a mudança
+        str r0, [r2]    @Escreve no registro
 .endm
 
 @ Chama a saida dos pinos do display
@@ -219,48 +221,56 @@
                 entryModeSet            @ move o cursor
 
                 add r9, #1
-                cmp r9, #32
+                cmp r9, #31
         bne loop @ se r9 for diferente de 0, continue
 .endm
 
 .global _start
 
 _start:
-	ldr R0, =fileName
-	mov R1, #0x1b0
-	orr R1, #0x006
-	mov R2, R1
-	mov R7, #sys_open
+	ldr r0, =fileName
+	mov r1, #0x1b0
+	orr r1, #0x006
+	mov r2, r1
+	mov r6, #sys_open
 	swi 0
-	movs R4, R0
+	movs r4, r0
 
-	ldr R5, =gpioaddr
-	ldr R5, [R5]
-	mov R1, #pagelen
-	mov R2, #(prot_read + prot_write)
-	mov R3, #map_shared
-	mov R0, #0
-	mov R7, #sys_map
+	ldr r5, =gpioaddr
+	ldr r5, [r5]
+	mov r1, #pagelen
+	mov r2, #(prot_read + prot_write)
+	mov r3, #map_shared
+	mov r0, #0
+	mov r6, #sys_map
 	swi 0
-	movs R8, R0
+	movs r8, r0
 
         setOut
         inicirDisplay
         entryModeSet
 
-        @teste
+        @teste                   
+                                 @ 1
         setDisplay 1, 0, 0, 1, 1 @ envia o primeiro conjunto de dados para o display
         setDisplay 1, 0, 0, 0, 1 @ envia o segundo conjunto de dados para o display
         
         entryModeSet                     @ move o cursor
-
+                                 @ 2
         setDisplay 1, 0, 0, 1, 1 @ envia o primeiro conjunto de dados para o display
         setDisplay 1, 0, 0, 1, 0 @ envia o segundo conjunto de dados para o display
 
         entryModeSet                     @ move o cursor
-
+                                @ 3
         setDisplay 1, 0, 0, 1, 1 @ envia o primeiro conjunto de dados para o display
         setDisplay 1, 0, 0, 1, 1 @ envia o segundo conjunto de dados para o display
+        
+        /*mov r6, #9
+        contador:
+                nanoSleep timeSecond       @ aguarda 1 segundo
+                sub r6, #1
+        cbnz contador       @ loop infinito*/
+        
         /*
         ldr r10, =texto        @ passa o valor do texto para r10
         mov r9, #0             @ tamanho do texto
@@ -277,15 +287,24 @@ _start:
                         casos:
                         @ se for 0 seta no pino DB4
                         cbz r0, case1
+                        cmp r0, #4
+                        beq case1
                         @ se for 1 seta no pino DB5
                         cmp r0, #1
+                        beq case2
+                        cmp r0, #5
                         beq case2
                         @ se for 2 seta no pino DB6
                         cmp r0, #2
                         beq case3
+                        cmp r0, #6
+                        beq case3
                         @ se for 3 seta no pino DB7
                         cmp r0, #3
                         beq case4
+                        cmp r0, #7
+                        beq case4
+
                         case1:
                                 GPIOValue pinE, #0 @ atribui 0 ao enable
                                 GPIOValue pinRS, #1         
@@ -324,13 +343,27 @@ _end:
         texto: .asciz "123456789"
         len_texto = .-texto
 
+        timeSecond: .word 1
         timespecnano5: .word 5000000
         timespecnano150: .word 150000
 
 	fileName: .asciz "/dev/mem"
 	gpioaddr: .word 0x20200 @carrega o endereco os onde registradores do controlador GPIO são mapeados na memória
 
+        @ pino do LED
+        pin6:   .word 0
+                .word 18
+                .word 6
 
+        @ pinos dos botoes
+        pin19:  .word 4
+                .word 27
+                .word 524288
+        pin26:  .word 8
+                .word 18
+                .word 67108864 
+
+        @ pinos do display LCD
         pinRS:	@ Pino RS - GPIO25
 		.word 8
 		.word 15

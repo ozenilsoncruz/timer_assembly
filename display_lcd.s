03b71d4cb2de15c38e30c9945d4871f278627e70
Antônio Neto <acm.neto1999@gmail.com>
2022-09-21 22:55:50 -0300

    será ?

--- display_lcd.s
+++ display_lcd.s
@@ -25,18 +25,6 @@
         GPIOValue pinE, #0              @ atribui 0 ao enble
 .endm
 
-@ Ativa ou desativa os pinos do display LCD
-.macro setDisplay2 RS, dados
-        GPIOValue pinE, #0              @ atribui 0 ao enable
-        GPIOValue pinRS, #\RS          
-        GPIOValue pinE, #1              @ atribui 1 ao enable
-        GPIOValue pinDB7, #\DB7   
-        GPIOValue pinDB6, #\DB6   
-        GPIOValue pinDB5, #\DB5 
-        GPIOValue pinDB4, #\DB4 
-        GPIOValue pinE, #0              @ atribui 0 ao enble
-.endm
-
 @ Modo de configuração de entrada
 .macro entryModeSet
         setDisplay 0, 0, 0, 0, 0 
@@ -145,22 +133,24 @@ _start:
                                     Carrega o byte na posicao indicada*/
             mov r0, #0
             mov r1, #1
-
+           
             loop2:  @ percorre todos os 8 bits do bit para sabe o nivel logico
-                ldr r3
-                and r2, r1, r3 @ faz um and entre r1 e r3 para saber se o bit esta ativo ou nao
-                
-                @ verificar logica >>> str r8, [#?, ?] @ adiciona o valor de r2 na memoria
+                and r2, r1, r11 @ faz um and entre r1 e r1 para saber se o bit esta ativo ou nao
+
+                cmp r2, #0 @ compara o 
+
+                beq @ manda 0 para o pino de saida
+
+
 
                 lsl r1, #1 /* desloca o bit para a esquerda
                               ex: 0001 -> 0010 */
+                
                 add r0, #1
                 cmp r0, #7
             beq loop2
 
-            setDisplay 1, DB7, DB6, DB5, DB4 @ envia o primeiro conjunto de dados para o display
-            setDisplay 1, DB7, DB6, DB5, DB4 @ envia o segundo conjunto de dados para o display
-            entryModeSet                     @ move o cursor
+            entryModeSet @ move o cursor
 
             add r9, #1
             cmp r9, #len_texto
